0fbe990f020f2ea958fc0f02ca460bbfc92412cc
Anders Martinsen <anders.martinsen@bekk.no>
2012-03-23 16:38:51 +0100

    [Kaizen] Endre rekkefÃ¸lgen pÃ¥ oppgavene i js-fila slik at de nÃ¥ ligger kronologisk utenom oppgave 5 som lÃ¸ses i metoden for setupBindings.

--- www/javascripts/app-oppgave.js
+++ www/javascripts/app-oppgave.js
@@ -45,28 +45,21 @@
 		// Lag et Media-objekt av opptaket og spill det av. Du velger selv om du vil lage en ny metode i App for dette, eller om du bare vil legge det direkte på riktig sted i setupBindings().
       });
     },
-	renderTweets: function(tweets){
-	      var self = this;
-	      $.each(tweets, function(i, tweet) {
-	        if(tweet.text !== undefined) {
-	          var tweet_html = '<li><h1>Tweet</h1><p>' + tweet.text + '</p><\/li>';
-	          self.tweets.append(tweet_html);
-	        }
-	      });
-	      self.tweets.listview("refresh");
-	},
-	renderUser: function(user){
-		var self = this;
-		var user_html = '<img src="' + user.profile_image_url_https + '"/>'
-		user_html += '<p>Navn: ' + user.name + '</p>';
-		user_html += '<p>Følgere: ' + user.followers_count + '</p>'
-		user_html += '<p>Følger: ' + user.friends_count + '</p>'
-		user_html += '<a href="" class="save" onClick="lagreAnsattTilKontaktLista(); return false;" class="ui-btn-right" data-role="button" data-icon="check">Save</a>';
-		self.user.append(user_html);
-    self.user.find("a.save").on("click", function(){
-      self.lagreAnsattTilKontaktLista(user);
-    });
-  },
+	search: function(keyword) {
+		/*
+		Oppgave 1
+		Søk etter keywords på twitter og vis dem i en liste med tweets. Søkefeltet og søkeknappen er allerede knyttet opp til riktige metoder i App-objektet. 
+		Du må altså hente ut søkeordet fra søkefeltet og bruke $.ajax() til å hente svar i JSON-format. Dette sender du så til renderTweets(tweets) og bytter til 
+		resultatsiden (som også er ett attributt på App).
+		*/
+     	var searchUrl = 'http://search.twitter.com/search.json?callback=?&q=' + keyword;
+
+	      // Bruk $.ajax() til å søke etter tweets
+
+	      // Bytt til resultatsiden og vis tweets
+	      $.mobile.changePage(self.resultsPage);
+	      self.renderTweets(data.results);
+    },
 	searchByLocation: function() {
 		/*	
 			Oppgave 2
@@ -84,65 +77,72 @@
 	      $.mobile.changePage(self.resultsPage);
 	      self.renderTweets(data.results);
 	},
-	searchByLocationWithNetworkCheck: function() {
+	searchByTwitterName: function(username) {
 		/*
-			Oppgave 4
-			Dette er en utvidelse av oppgave 2 hvor man før man henter tweets basert på geolocation, sjekker om telefonen har nettverk. Dette gjøres via PhoneGap sitt Connection-API.
-			Hvis man ikke har nettverk, bruk PhoneGap sitt Notification-API til å gi en fornuftig feilmelding. Ellers vis tweets by geolokasjon.
+		Oppgave 3, første del
+		Her skal man kunne søke etter et brukernavn på twitter og så legge til kontaktinfo (bilde og navn i det minste) i kontaktlista på telefonen.
+		I denne metoden lager du selve twitter-søket. Metoden renderUser() tar inn objektet du får fra twitter og skriver du ut i html'en.
+		I lagreAnsattTilKontaktLista() bruker du phonegap til å lagre brukeren i kontaktlista. Husk å bytt til "vis-bruker"-siden.
 		*/
-		console.log("Searching for geolocation with network check")
-		var self = this;
-		checkConnection();
-	    
-		function checkConnection() {
-			// Bruk PhoneGap til å sjekke om du har tilgang til nettverk
-			// Hvis nei, hvis feilmelding, ellers vis tweets by geolocation med metoden findGeoLocationWithPhoneGap()
-		}
 		
-		function alertDismissed() {
-			console.log("Alert dismissed");
-		}
-		
-		function findGeoLocationWithPhoneGap() {
-			// Denne metoden blir lik som metoden searchByLocation() som du fullførte i oppgave 2
-		}
-		},
-		searchByTwitterName: function(username) {
+		var searchUrl = 'http://api.twitter.com/1/users/show.json?callback=?&screen_name=' + username;
+
+	      // Søk etter bruker vha. $.ajax()
+
+	      // Bytt til brukerinfosiden når søkeresultatet kommer
+	      $.mobile.changePage(self.usernameResultPage);
+	      self.renderUser(data); 
+	  	},
+		lagreAnsattTilKontaktLista: function(user) {
+			// Oppgave 3, andre del
+			// Denne delen består av implementere metoden searchByTwitterName() som ligg
+	    	// Bruk kontakt-API'et til PhoneGap å lagre informasjonen i "user"-objektet til kontaktlista, f.eks bilde og navn
+	    },
+		searchByLocationWithNetworkCheck: function() {
 			/*
-			Oppgave 3, første del
-			Her skal man kunne søke etter et brukernavn på twitter og så legge til kontaktinfo (bilde og navn i det minste) i kontaktlista på telefonen.
-			I denne metoden lager du selve twitter-søket. Metoden renderUser() tar inn objektet du får fra twitter og skriver du ut i html'en.
-			I lagreAnsattTilKontaktLista() bruker du phonegap til å lagre brukeren i kontaktlista. Husk å bytt til "vis-bruker"-siden.
+				Oppgave 4
+				Dette er en utvidelse av oppgave 2 hvor man før man henter tweets basert på geolocation, sjekker om telefonen har nettverk. Dette gjøres via PhoneGap sitt Connection-API.
+				Hvis man ikke har nettverk, bruk PhoneGap sitt Notification-API til å gi en fornuftig feilmelding. Ellers vis tweets by geolokasjon.
 			*/
-			
-			var searchUrl = 'http://api.twitter.com/1/users/show.json?callback=?&screen_name=' + username;
+			console.log("Searching for geolocation with network check")
+			var self = this;
+			checkConnection();
 
-		      // Søk etter bruker vha. $.ajax()
+			function checkConnection() {
+				// Bruk PhoneGap til å sjekke om du har tilgang til nettverk
+				// Hvis nei, hvis feilmelding, ellers vis tweets by geolocation med metoden findGeoLocationWithPhoneGap()
+			}
 
-		      // Bytt til brukerinfosiden når søkeresultatet kommer
-		      $.mobile.changePage(self.usernameResultPage);
-		      self.renderUser(data); 
-		  	},
-			lagreAnsattTilKontaktLista: function(user) {
-				// Oppgave 3, andre del
-				// Denne delen består av implementere metoden searchByTwitterName() som ligg
-		    	// Bruk kontakt-API'et til PhoneGap å lagre informasjonen i "user"-objektet til kontaktlista, f.eks bilde og navn
-		    },
-    search: function(keyword) {
-		/*
-		Oppgave 1
-		Søk etter keywords på twitter og vis dem i en liste med tweets. Søkefeltet og søkeknappen er allerede knyttet opp til riktige metoder i App-objektet. 
-		Du må altså hente ut søkeordet fra søkefeltet og bruke $.ajax() til å hente svar i JSON-format. Dette sender du så til renderTweets(tweets) og bytter til 
-		resultatsiden (som også er ett attributt på App).
-		*/
-     	var searchUrl = 'http://search.twitter.com/search.json?callback=?&q=' + keyword;
+			function alertDismissed() {
+				console.log("Alert dismissed");
+			}
 
-	      // Bruk $.ajax() til å søke etter tweets
-
-	      // Bytt til resultatsiden og vis tweets
-	      $.mobile.changePage(self.resultsPage);
-	      self.renderTweets(data.results);
-    }
+			function findGeoLocationWithPhoneGap() {
+				// Denne metoden blir lik som metoden searchByLocation() som du fullførte i oppgave 2
+			}
+	},
+	renderTweets: function(tweets) {
+	      var self = this;
+	      $.each(tweets, function(i, tweet) {
+	        if(tweet.text !== undefined) {
+	          var tweet_html = '<li><h1>Tweet</h1><p>' + tweet.text + '</p><\/li>';
+	          self.tweets.append(tweet_html);
+	        }
+	      });
+	      self.tweets.listview("refresh");
+	},
+	renderUser: function(user){
+		var self = this;
+		var user_html = '<img src="' + user.profile_image_url_https + '"/>'
+		user_html += '<p>Navn: ' + user.name + '</p>';
+		user_html += '<p>Følgere: ' + user.followers_count + '</p>'
+		user_html += '<p>Følger: ' + user.friends_count + '</p>'
+		user_html += '<a href="" class="save" onClick="lagreAnsattTilKontaktLista(); return false;" class="ui-btn-right" data-role="button" data-icon="check">Save</a>';
+		self.user.append(user_html);
+    	self.user.find("a.save").on("click", function(){
+      self.lagreAnsattTilKontaktLista(user);
+    });
+  },
   });
   window.App = App;
 })();
